0d50945b190a7021e5ce4dbf3339e49f045ebb55
Jose72 <jjjtony_72@hotmail.com>
2017-05-11 15:03:32 -0300

    code for terrain

--- z/server/terrain.cpp
+++ z/server/terrain.cpp
@@ -1,7 +1,26 @@
 #include "terrain.h"
 
+
+terrain::terrain(int t_type, bool pb_robot, bool pb_vehicle, double t_factor): 
+t_type(t_type), passable_by_robot(pb_robot), passable_by_vehicle(pb_vehicle), 
+terrain_factor(t_factor) {};
+
 bool terrain::isPassable(int unit_code){
 	if (unit_code == ROBOT) return passable_by_robot;
 	if (unit_code == VEHICLE) return passable_by_vehicle;
 	return false;
 }
+
+
+
+
+//constructores de los terrenos
+
+terrainPradera::terrainPradera(): terrain(PRADERA, true, true, 
+PRADERA_FACTOR) {};
+
+terrainAgua::terrainAgua(): terrain(AGUA, true, false, 
+AGUA_FACTOR) {};
+
+terrainLava::terrainLava(): terrain(LAVA, false, false, 
+LAVA) {};
--- z/server/terrain.h
+++ z/server/terrain.h
@@ -1,18 +1,58 @@
 #ifndef TERRAIN_H
 #define TERRAIN_H
 
-//para ver si pasan por el terreno
+//codigo de unidad para ver si pasan por el terreno
 #define ROBOT 4
 #define VEHICLE 5
 
+//esto en un xml?
+#define TIERRA 6
+#define PRADERA 7
+#define NIEVE 8
+#define AGUA 9
+#define PANTANO 10
+#define LAVA 11
+#define CARRETERA 12
+#define ASFALTO 13
+
+#define TIERRA_FACTOR 1
+#define PRADERA_FACTOR 1
+#define NIEVE_FACTOR 1
+#define AGUA_FACTOR 0.7
+#define PANTANO_FACTOR 0.7
+#define LAVA_FACTOR 0
+#define CARRETERA_FACTOR 1.5
+#define ASFALTO_FACTOR 1.5
+
 class terrain{
-	private:
+	protected:
+		int t_type; //tipo de terreno, o codigo de terreno
 		bool passable_by_robot;
 		bool passable_by_vehicle;
 		double terrain_factor; //factor de velocidad en el terreno
 	public:
+		terrain(int t_type, bool pb_robot, bool pb_vehicle, double t_factor);
 		bool isPassable(int unit_code);
 		double getTerrainFactor();
 };
+
+
+//tipos de terrenos
+
+class terrainPradera: public terrain{
+	public:
+		terrainPradera();
+};
+
+class terrainAgua: public terrain{
+	public:
+		terrainAgua();
+};
+
+class terrainLava: public terrain{
+	public:
+		terrainLava();
+};
+
 
 #endif
