ed5195fb755ded89c077fa825a1cadf8a49dfb8b
Matias Bataglia <matubat91@gmail.com>
2017-05-28 22:02:04 -0300

    Unidad se mueve relativa a la camara

--- z/client/Camera.cpp
+++ z/client/Camera.cpp
@@ -32,8 +32,13 @@ void Camera::show(Units_Protected &units, Game_map &game_map) {
     flag->animate();
     game_map.draw_map(limitXL,limitXR,limitYU,limitYD);
     for (int i = 0; i <units.size() ; ++i) {
-        if(BETWEEN(units[i]->get_cameraPosX(),limitXL,limitXR)){
-            if(BETWEEN(units[i]->get_cameraPosY(),limitYU,limitYD)){
+        /** if(BETWEEN(units[i]->get_cameraPosX(),limitXL,limitXR)){
+             if(BETWEEN(units[i]->get_cameraPosY(),limitYU,limitYD)){
+                 units[i]->animate();
+             }
+         }*/
+        if(units[i]->get_cameraPosX() >= limitXL && units[i]->get_cameraPosX() <= limitXR){
+            if(units[i]->get_cameraPosY() >= limitYU && units[i]->get_cameraPosY() <= limitYD){
                 units[i]->animate();
             }
         }
@@ -46,29 +51,30 @@ void Camera::set_relative_position(Units_Protected &units) {
     for (int i = 0; i <units.size() ; ++i) {
         int posUnitX = units[i]->get_posx();
         int posUnitY = units[i]->get_posy();
+        //sin este if se muestra el grunt al iniciar el juego
+        //se podria quitar si solamente se llega a este codigo al modificar la camara
+        // if((posCameraX != posCameraXOld) || (posCameraY != posCameraYOld)) {
+        int posRUniX = posUnitX + posCameraX;
+        int posRUniY = posUnitY + posCameraY;
 
-        if((posCameraX != posCameraXOld) || (posCameraY != posCameraYOld)) {
-            int posRUniX = posUnitX - posCameraX;
-            int posRUniY = posUnitY - posCameraY;
+        // std::cout << posRUniX << "\n";
+        // std::cout << posRUniY << "\n";
+        /** if ((posRUniX < 0) && (posRUniY < 0)) {
+             posUnitX = posUnitX + 20;
+             posUnitY = posUnitY + 20;
+         } else if ((posRUniX < 0) && (posRUniY > 0)) {
+             posUnitX = posUnitX + 20;
+             posUnitY = posUnitY - 20;
+         } else if ((posRUniX > 0) && (posRUniY > 0)) {
+             posUnitX = posUnitX - 20;
+             posUnitY = posUnitY - 20;
+         } else if ((posRUniX > 0) && (posRUniY < 0)) {
+             posUnitX = posUnitX - 20;
+             posUnitY = posUnitY + 20;
+         }**/
+        units[i]->set_pos_camera(posRUniX,posRUniY);
 
-            std::cout << posRUniX << "\n";
-            std::cout << posRUniY << "\n";
-            if ((posRUniX < 0) && (posRUniY < 0)) {
-                posUnitX = posUnitX + 20;
-                posUnitY = posUnitY + 20;
-            } else if ((posRUniX < 0) && (posRUniY > 0)) {
-                posUnitX = posUnitX + 20;
-                posUnitY = posUnitY - 20;
-            } else if ((posRUniX > 0) && (posRUniY > 0)) {
-                posUnitX = posUnitX - 20;
-                posUnitY = posUnitY - 20;
-            } else if ((posRUniX > 0) && (posRUniY < 0)) {
-                posUnitX = posUnitX - 20;
-                posUnitY = posUnitY + 20;
-            }
-            units[i]->set_pos_camera(posUnitX,posUnitY);
-
-        }
+        // }
         //units[i]->set_pos(posUnitX,posUnitY);
 
     }
