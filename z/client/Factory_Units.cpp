06ac78e7ff7e63ebd700fa97ea8045207bbf4b20
Matias R <matiasreimondo@gmail.com>
2017-06-07 08:20:23 -0300

    Fix de animaciones

--- z/client/Factory_Units.cpp
+++ z/client/Factory_Units.cpp
@@ -3,11 +3,7 @@
 Factory_Units::Factory_Units(SpritesPool &pool):pool(pool) {}
 
 Unit* Factory_Units::createUnit(FlagsUnitType unit,int unit_code, int posx, int posy) {
-    std::vector<Animation*> animation;
-    std::vector<Animation*> animation2;
-    std::vector<Animation*> animation3;
-    std::vector<Animation*> animation4;
-    std::vector<Animation*> animation5;
+
     switch (unit){
         case BLUE_GRUNT:
             return new Unit(this->pool.get_animations(BLUE_GRUNT),
@@ -16,56 +12,74 @@ Unit* Factory_Units::createUnit(FlagsUnitType unit,int unit_code, int posx, int 
                             this->pool.get_animations(BEER_BLUE),
                             this->pool.get_animations(CELEBRATE_BLUE),unit_code,posx,posy);
         case GREEN_GRUNT:
-            animation  = this->pool.get_animations(GREEN_GRUNT);
-            animation2 = this->pool.get_animations(FIRE_GREEN_GRUNT);
-            animation3 = this->pool.get_animations(DIE_GREEN_GRUNT);
-            animation4 = this->pool.get_animations(BEER_GREEN);
-            animation5 = this->pool.get_animations(CELEBRATE_GREEN);
-            return new Unit(animation,animation2,animation3,animation4,animation5,unit_code,posx,posy);
+            return new Unit (this->pool.get_animations(GREEN_GRUNT),
+                             this->pool.get_animations(FIRE_GREEN_GRUNT),
+                             this->pool.get_animations(DIE_GREEN_GRUNT),
+                             this->pool.get_animations(BEER_GREEN),
+                             this->pool.get_animations(CELEBRATE_GREEN),
+                             unit_code,posx,posy);
         case RED_GRUNT:
-            animation  = this->pool.get_animations(RED_GRUNT);
-            animation2 = this->pool.get_animations(FIRE_RED_GRUNT);
-            animation3 = this->pool.get_animations(DIE_RED_GRUNT);
-            animation4 = this->pool.get_animations(BEER_RED);
-            animation5 = this->pool.get_animations(CELEBRATE_RED);
-            return new Unit(animation,animation2,animation3,animation4,animation5,unit_code,posx,posy);
+            return new Unit(this->pool.get_animations(RED_GRUNT),
+                            this->pool.get_animations(FIRE_RED_GRUNT),
+                            this->pool.get_animations(DIE_RED_GRUNT),
+                            this->pool.get_animations(BEER_RED),
+                            this->pool.get_animations(CELEBRATE_RED),
+                            unit_code,posx,posy);
 
         case YELLOW_GRUNT:
-            animation  = this->pool.get_animations(YELLOW_GRUNT);
-            animation2 = this->pool.get_animations(FIRE_YELLOW_GRUNT);
-            animation3 = this->pool.get_animations(DIE_YELLOW_GRUNT);
-            animation4 = this->pool.get_animations(BEER_YELLOW);
-            animation5 = this->pool.get_animations(CELEBRATE_YELLOW);
-            return new Unit(animation,animation2,animation3,animation4,animation5,unit_code,posx,posy);
+            return new Unit(this->pool.get_animations(YELLOW_GRUNT),
+                            this->pool.get_animations(FIRE_YELLOW_GRUNT),
+                            this->pool.get_animations(DIE_YELLOW_GRUNT),
+                            this->pool.get_animations(BEER_YELLOW),
+                            this->pool.get_animations(CELEBRATE_YELLOW),
+                            unit_code,posx,posy);
 
         case MISILE_LAUNCHER_BLUE:
-            animation = this->pool.get_animations(MISILE_LAUNCHER_BLUE);
-            return new Unit(animation,animation,animation,animation,animation,unit_code,posx,posy,ROLLING);
+            return new Unit(this->pool.get_animations(MISILE_LAUNCHER_BLUE),
+                            this->pool.get_animations(MISILE_LAUNCHER_BLUE),
+                            this->pool.get_animations(MISILE_LAUNCHER_BLUE),
+                            this->pool.get_animations(MISILE_LAUNCHER_BLUE),
+                            this->pool.get_animations(MISILE_LAUNCHER_BLUE),
+                            unit_code,posx,posy,ROLLING);
 
         case MISILE_LAUNCHER_GREEN:
-            animation = this->pool.get_animations(MISILE_LAUNCHER_GREEN);
-            return new Unit(animation,animation,animation,animation,animation,unit_code,posx,posy,ROLLING);
+            return  new Unit(this->pool.get_animations(MISILE_LAUNCHER_GREEN),
+                             this->pool.get_animations(MISILE_LAUNCHER_GREEN),
+                             this->pool.get_animations(MISILE_LAUNCHER_GREEN),
+                             this->pool.get_animations(MISILE_LAUNCHER_GREEN),
+                             this->pool.get_animations(MISILE_LAUNCHER_GREEN),
+                             unit_code,posx,posy,ROLLING);
 
         case MISILE_LAUNCHER_RED:
-            animation = this->pool.get_animations(MISILE_LAUNCHER_RED);
-            return new Unit(animation,animation,animation,animation,animation,unit_code,posx,posy,ROLLING);
+            return new Unit(this->pool.get_animations(MISILE_LAUNCHER_RED),
+                            this->pool.get_animations(MISILE_LAUNCHER_RED),
+                            this->pool.get_animations(MISILE_LAUNCHER_RED),
+                            this->pool.get_animations(MISILE_LAUNCHER_RED),
+                            this->pool.get_animations(MISILE_LAUNCHER_RED),
+                            unit_code,posx,posy,ROLLING);
 
         case MISILE_LAUNCHER_YELLOW:
-            animation = this->pool.get_animations(MISILE_LAUNCHER_YELLOW);
-            return new Unit(animation,animation,animation,animation,animation,unit_code,posx,posy,ROLLING);
+            return  new Unit(this->pool.get_animations(MISILE_LAUNCHER_YELLOW),
+                             this->pool.get_animations(MISILE_LAUNCHER_YELLOW),
+                             this->pool.get_animations(MISILE_LAUNCHER_YELLOW),
+                             this->pool.get_animations(MISILE_LAUNCHER_YELLOW),
+                             this->pool.get_animations(MISILE_LAUNCHER_YELLOW),
+                             unit_code,posx,posy,ROLLING);
 
         case COLORLESS_FLAG:
-            animation  = this->pool.get_animations(COLORLESS_FLAG);
-            animation2 = this->pool.get_animations(BLUE_FLAG);
-            animation3 = this->pool.get_animations(RED_FLAG);
-            animation4 = this->pool.get_animations(GREEN_FLAG);
-            animation5 = this->pool.get_animations(YELLOW_FLAG);
-            return new Unit(animation,animation2,animation3,animation4,animation5,unit_code,posx,posy,COLORLESS);
+            return new Unit(this->pool.get_animations(COLORLESS_FLAG),
+                            this->pool.get_animations(BLUE_FLAG),
+                            this->pool.get_animations(RED_FLAG),
+                            this->pool.get_animations(GREEN_FLAG),
+                            this->pool.get_animations(YELLOW_FLAG),
+                            unit_code,posx,posy,COLORLESS);
         case FORT_SPRITE:
-            animation = this->pool.get_animations(FORT_SPRITE);
-            return new Unit(animation,animation,animation,animation,animation,unit_code,posx,posy);
-
-
+            return new Unit(this->pool.get_animations(FORT_SPRITE),
+                            this->pool.get_animations(FORT_SPRITE),
+                            this->pool.get_animations(FORT_SPRITE),
+                            this->pool.get_animations(FORT_SPRITE),
+                            this->pool.get_animations(FORT_SPRITE),
+                            unit_code,posx,posy);
     }
 
 }
